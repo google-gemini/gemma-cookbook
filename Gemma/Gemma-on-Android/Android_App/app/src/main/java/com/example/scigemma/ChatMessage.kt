739de5eef3044c6bd69fa0db2c07924750ebfb3c
NSTiwari <tiwarinitin1999@gmail.com>
2024-08-09 13:01:27 +0530

    Added example for on-device deployment of fine-tuned Gemma-2B-it on Android.

---
+++ Gemma/Gemma-on-Android/Android_App/app/src/main/java/com/example/scigemma/ChatMessage.kt
@@ -0,0 +1,16 @@
+package com.example.scigemma
+
+import java.util.UUID
+
+/**
+ * Used to represent a ChatMessage
+ */
+data class ChatMessage(
+    val id: String = UUID.randomUUID().toString(),
+    val message: String = "",
+    val author: String,
+    val isLoading: Boolean = false
+) {
+    val isFromUser: Boolean
+        get() = author == USER_PREFIX
+}
