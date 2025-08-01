99918e8ad0ed081ae4ee64d448801760c9ffce0a
Juyeong Ji <bebechien@gmail.com>
2025-08-01 19:23:44 +0900

    Simple text example

---
+++ Workshops/Android/GemmaAppText/app/src/main/java/com/example/gemmaapptext/ChatViewModel.kt
@@ -0,0 +1,73 @@
+package com.example.gemmaapptext
+
+import android.content.Context
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.CreationExtras
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+private const val TAG = "ChatViewModel"
+
+class ChatViewModel(
+    private val llmModel: GemmaModel
+) : ViewModel() {
+    private val _uiState: MutableStateFlow<UiState> =
+        MutableStateFlow(UiState.Initial)
+    val uiState: StateFlow<UiState> =
+        _uiState.asStateFlow()
+
+    fun resetSession() {
+        llmModel.resetSession()
+    }
+
+    /*fun init(context: Context, updateResult: (m: String) -> Unit) {
+        llmModel.init(context, updateResult)
+    }*/
+
+    fun sendPrompt(
+        prompt: String,
+        updateResult: (m: String) -> Unit
+    ) {
+        _uiState.value = UiState.Loading
+
+        viewModelScope.launch(Dispatchers.Default) {
+            try {
+                var response = ""
+                llmModel.generateResponseAsync(prompt, { partialResult, done ->
+                    response += partialResult
+                    updateResult(response)
+                    if (done) {
+                        _uiState.value = UiState.Success(response)
+                    } else {
+                        _uiState.value = UiState.Generating
+                    }
+                })
+            } catch (e: Exception) {
+                _uiState.value = UiState.Error(e.localizedMessage ?: "")
+            }
+        }
+    }
+
+    fun stopRespones() {
+        Log.d(TAG, "Stopping response for model...")
+        viewModelScope.launch(Dispatchers.Default) {
+            llmModel.cancelGenerateResponseAsync()
+        }
+        Log.d(TAG, "Stopping done.")
+    }
+
+    companion object {
+        fun getFactory(context: Context) = object : ViewModelProvider.Factory {
+            override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
+                val inferenceModel = GemmaModel.getInstance(context)
+                return ChatViewModel(inferenceModel) as T
+            }
+        }
+    }
+}
---
+++ Workshops/Android/GemmaAppText/app/src/main/java/com/example/gemmaapptext/GemmaModel.kt
@@ -0,0 +1,108 @@
+package com.example.gemmaapptext
+
+import android.content.Context
+import android.util.Log
+import com.google.mediapipe.tasks.genai.llminference.LlmInference
+import com.google.mediapipe.tasks.genai.llminference.LlmInferenceSession
+import java.io.File
+
+private const val TAG = "GemmaModel"
+
+// Default values for LLM models
+private object LLMConstants {
+    const val MODEL_PATH = "/data/local/tmp/llm/gemma-3n-E2B-it-int4.task"
+    const val DEFAULT_MAX_TOKEN = 512
+    const val DEFAULT_TOPK = 40
+    const val DEFAULT_TOPP = 0.9f
+    const val DEFAULT_TEMPERATURE = 1.0f
+    val DEFAULT_BACKEND = LlmInference.Backend.GPU
+}
+
+typealias ResultListener = (partialResult: String, done: Boolean) -> Unit
+
+class GemmaModel private constructor(context: Context) {
+    private lateinit var llmInference: LlmInference
+    private lateinit var llmInferenceSession: LlmInferenceSession
+
+    init {
+        if (!modelExists()) {
+            Log.d(TAG, "Model not found at path: ${LLMConstants.MODEL_PATH}")
+            //throw IllegalArgumentException("Model not found at path: ${LLMConstants.MODEL_PATH}")
+        } else {
+
+            // Set the configuration options for the LLM Inference task
+            val taskOptions = LlmInference.LlmInferenceOptions.builder()
+                .setModelPath(LLMConstants.MODEL_PATH)
+                .setMaxTokens(LLMConstants.DEFAULT_MAX_TOKEN)
+                .setPreferredBackend(LLMConstants.DEFAULT_BACKEND)
+                .build()
+
+            // Create an instance of the LLM Inference task
+            llmInference = LlmInference.createFromOptions(context, taskOptions)
+            createSession()
+        }
+    }
+
+    fun close() {
+        llmInferenceSession.close()
+        llmInference.close()
+    }
+
+    fun resetSession() {
+        if (!modelExists()) {
+            Log.d(TAG, "Model not found at path: ${LLMConstants.MODEL_PATH}")
+            return
+        }
+
+        llmInferenceSession.close()
+        createSession()
+    }
+
+    private fun createSession() {
+        llmInferenceSession =
+            LlmInferenceSession.createFromOptions(
+                llmInference,
+                LlmInferenceSession.LlmInferenceSessionOptions.builder()
+                    .setTopK(LLMConstants.DEFAULT_TOPK)
+                    .setTopP(LLMConstants.DEFAULT_TOPP)
+                    .setTemperature(LLMConstants.DEFAULT_TEMPERATURE)
+                    .build(),
+            )
+    }
+
+    fun generateResponseAsync(
+        prompt: String,
+        resultListener: ResultListener
+    ) {
+        if (!modelExists()) {
+            throw Exception("Model not found at path: ${LLMConstants.MODEL_PATH}")
+        }
+
+        llmInferenceSession.addQueryChunk(prompt)
+        llmInferenceSession.generateResponseAsync(resultListener)
+    }
+
+    fun cancelGenerateResponseAsync() {
+        llmInferenceSession.cancelGenerateResponseAsync()
+    }
+
+    companion object {
+        private var instance: GemmaModel? = null
+
+        fun getInstance(context: Context): GemmaModel {
+            return if (instance != null) {
+                instance!!
+            } else {
+                GemmaModel(context).also { instance = it }
+            }
+        }
+
+        fun resetInstance(context: Context): GemmaModel {
+            return GemmaModel(context).also { instance = it }
+        }
+
+        fun modelExists(): Boolean {
+            return File(LLMConstants.MODEL_PATH).exists()
+        }
+    }
+}
