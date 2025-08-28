41aecd22123f4224d43e0d4bd6e54e39c56027b7
Sitam Meur <103279526+sitammeur@users.noreply.github.com>
2025-08-28 06:38:21 +0000

    fix progress calculation

--- Demos/Gemma3-on-Web/src/components/Progress.jsx
+++ Demos/Gemma3-on-Web/src/components/Progress.jsx
@@ -1,5 +1,5 @@
 function formatBytes(size) {
-  const i = size == 0 ? 0 : Math.floor(Math.log(size) / Math.log(1024));
+  const i = size === 0 ? 0 : Math.floor(Math.log(size) / Math.log(1024));
   return (
     +(size / Math.pow(1024, i)).toFixed(2) * 1 +
     ["B", "kB", "MB", "GB", "TB"][i]
