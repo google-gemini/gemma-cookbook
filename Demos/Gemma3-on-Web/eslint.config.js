3c2e9def95a9fd047079cf37d65397f5749ffa0e
Sitam Meur <103279526+sitammeur@users.noreply.github.com>
2025-08-28 05:17:29 +0000

    fix eslint package config

--- Demos/Gemma3-on-Web/eslint.config.js
+++ Demos/Gemma3-on-Web/eslint.config.js
@@ -17,7 +17,7 @@ export default [
         sourceType: 'module',
       },
     },
-    settings: { react: { version: '18.3' } },
+    settings: { react: { version: 'detect' } },
     plugins: {
       react,
       'react-hooks': reactHooks,
